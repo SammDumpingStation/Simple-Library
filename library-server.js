74931576d5a03f50aaf20a5abbb89a3a32c68b22
Samm Caagbay <sammcaag@gmail.com>
2024-04-17 13:31:46 +0800

    🔥 Usin MySQL as my database and integrating it into EXPRESS.JS

--- library-server.js
+++ library-server.js
@@ -1,18 +1,28 @@
 const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
+const mysql = require("mysql2");
+
 const app = express();
 
-const dbURI =
-  "mongodb+srv://caagbaysamm:Golem42069@librarymanagementsystem.fdtzg6f.mongodb.net/?retryWrites=true&w=majority&appName=LibraryManagementSystem";
-mongoose
-  .connect(dbURI)
-  .then((result) => {
-    console.log("connection successful");
-  })
-  .catch((err) => {
-    console.log(err);
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Golempekkawizard69",
+  database: "LibrarySystem",
+});
+
+pool.query("SELECT * FROM students", (error, results, fields) => {
+  if (error) {
+    console.error("Error executing SQL query:", error);
+    return;
+  }
+
+  results.forEach((student) => {
+    console.log(`ID: ${student.students_id}`);
+    console.log(`First Name: ${student.first_name}`);
+    console.log(`Last Name: ${student.last_name}`);
+    console.log("---");
   });
+});
 app.set("view engine", "ejs");
 
 app.listen(3000);
@@ -20,8 +30,6 @@ app.listen(3000);
 app.use(express.static("Public/CSS-Files"));
 app.use(express.static("Public/Image-Files"));
 app.use(express.static("Public/JS-Files"));
-
-// app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const students = [
